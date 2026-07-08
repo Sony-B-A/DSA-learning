b733f85b13724d7c2ff100586508edf7dbd49449
Sony-B-A <sonyshaiva444@gmail.com>
2026-07-08 11:48:46 +0530

    factorial

---
+++ gfg/13factorial.py
@@ -0,0 +1,7 @@
+# Given a positive integer, n. Find the factorial of n.
+
+    def factorial(self, n: int) -> int:
+        # code here
+        if n== 0 or n == 1:
+            return 1
+        return n * self.factorial(n - 1)
